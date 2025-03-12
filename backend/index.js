6e388aa5aa0345166162c5ef20e18f7020b92b51
Lekshmi <sreelekshmi310192@gmail.com>
2025-03-12 12:33:10 +0530

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGODB_URL, { serverSelectionTimeoutMS: 20000 })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server Running on http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`Server Running on http://localhost:${PORT}`);
 });
