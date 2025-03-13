61e471653bfc8fb567ac4b3f53ad12f5a02abc68
Lekshmi <sreelekshmi310192@gmail.com>
2025-03-13 19:55:23 +0530

    created login UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import SignupForm from "./pages/SignupForm";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return <>
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/signup' element={<SignupForm />} />
+    <Route path='/login' element={<LoginPage />} />
   </Routes>
   </>;
 }
