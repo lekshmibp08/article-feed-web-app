8a4bd74a6f66258ff3887d4c6280122782442a8c
Lekshmi <sreelekshmi310192@gmail.com>
2025-03-15 19:09:29 +0530

    settings page route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,28 @@ import { ToastContainer } from "react-toastify"; // Import ToastContainer
 import "react-toastify/dist/ReactToastify.css";
 
 
-import Home from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import SignupForm from "./pages/SignupForm";
 import LoginPage from "./pages/LoginPage"
 import DashboardPage from "./pages/DashboardPage";
 import CreateArticlePage from "./pages/CreateArticlePage";
 import MyArticlesPage from "./pages/MyArticlesPage";
 import EditArticlePage from "./pages/EditArticlePage";
+import SettingsPage from "./pages/SettingsPage";
+
+impo
 
 function App() {
   return <>
   <Routes>
-    <Route path='/' element={<Home />} />
+    <Route path='/' element={<HomePage />} />
     <Route path='/signup' element={<SignupForm />} />
     <Route path='/login' element={<LoginPage />} />
     <Route path='/dashboard' element={<DashboardPage />} />
     <Route path="/dashboard/create-article" element={<CreateArticlePage />} />
     <Route path="/dashboard/my-articles" element={<MyArticlesPage />} />
     <Route path="/dashboard/edit-article/:id" element={<EditArticlePage />} />
+    <Route path="/dashboard/settings" element={<SettingsPage />} />
 
 
   </Routes>
