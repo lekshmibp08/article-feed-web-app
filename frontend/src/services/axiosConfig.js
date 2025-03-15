cf711fa36dad0060fc067ff7e618821925fbc639
Lekshmi <sreelekshmi310192@gmail.com>
2025-03-15 15:36:44 +0530

    axios interceptor implimented

---
+++ frontend/src/services/axiosConfig.js
@@ -0,0 +1,35 @@
+import axiosInstance from "axios";
+import store from '../redux/store';
+
+// Create an Axios instance
+const configAxios = axiosInstance.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  timeout: 10000, 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add an interceptor to attach the token automatically
+configAxios.interceptors.request.use(
+  (config) => {
+    // Check if the request is to Cloudinary
+    if (config.url?.startsWith('https://api.cloudinary.com')) {
+      return config; // Skip adding Authorization header
+    }
+    const token = store.getState().auth.token;
+    console.log("Redux Token",token);
+    
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default configAxios;
